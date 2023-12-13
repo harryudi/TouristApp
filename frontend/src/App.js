c654bd106e23317e6134240305c4f2899546b9ef
harryudi <udiharrison23@gmail.com>
2023-12-13 15:04:54 +0100

    Map-box api added with new features to zoom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react';
+import Map, {Marker} from 'react-map-gl';
+import {Room} from "@material-ui/icons"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      initialViewState={{
+        longitude: 3.3792,
+        latitude: 6.5244,
+        zoom: 10
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+      <Marker
+      longitude={3.3792} 
+      latitude={6.5244} 
+      // offsetLeft= {-20}
+      // offsetTop={-10}
+      >
+        <Room style={{fontSize:visualViewport.zoom}}/>
+      <div>Here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</div>
+      {/* <img src="./public/images/pin.png" alt="" /> */}
+      </Marker>
+      </Map>
     </div>
   );
 }
